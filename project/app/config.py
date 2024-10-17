3c24aa265a31d72768065d86d34d1b91e397a086
Roger Mukai <10594369+MukaiGuy@users.noreply.github.com>
2024-10-17 17:53:03 -0400

    postgress

--- project/app/config.py
+++ project/app/config.py
@@ -4,10 +4,7 @@
 import logging
 from functools import lru_cache
 
-<<<<<<< HEAD
 from pydantic import AnyUrl
-=======
->>>>>>> 16e83fc342c710ba04d8ba3d5dbd5773072eea98
 from pydantic_settings import BaseSettings
 
 
@@ -17,10 +14,7 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = "dev"
     testing: bool = 0
-<<<<<<< HEAD
     database_url: AnyUrl = None
-=======
->>>>>>> 16e83fc342c710ba04d8ba3d5dbd5773072eea98
 
 @lru_cache()
 def get_settings() -> BaseSettings:
